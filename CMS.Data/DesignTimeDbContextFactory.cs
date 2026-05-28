47f5e09b6d5ca7b070bb8529088b1113a2076b68
DELL <tn8159744@gmail.com>
2026-05-28 20:11:11 +0700

    hoan thanh buoi 2

---
+++ CMS.Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CMS.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            // Hardcode chuỗi kết nối tại đây để phục vụ RIÊNG cho công cụ Migration lúc gõ lệnh
+            var connectionString = "Server=.;Database=ThaiCMS_DB;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True";
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
