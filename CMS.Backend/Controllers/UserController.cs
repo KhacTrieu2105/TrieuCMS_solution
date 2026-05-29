2399a4719baeed152e362e683760367a53b75cc3
DELL <tn8159744@gmail.com>
2026-05-29 11:15:11 +0700

    bo sung b2

---
+++ CMS.Backend/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using CMS.Backend; // Tên namespace chứa ApplicationDbContext của bạn
+using CMS.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CMS.Backend.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        // Tiêm ApplicationDbContext qua Constructor
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Action Index lấy danh sách thành viên
+        public IActionResult Index()
+        {
+            var users = _context.Users.ToList();
+            return View(users);
+        }
+    }
+}
